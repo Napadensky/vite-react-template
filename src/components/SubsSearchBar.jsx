b69a0c14335f775277c0a94591d17bfc313d0ed2
napadensky <davidnapadensky@gmail.com>
2024-09-06 01:21:36 -0300

    add searchbar

---
+++ src/components/SubsSearchBar.jsx
@@ -0,0 +1,32 @@
+const stylesSubsSearchBar =
+  'm-auto flex h-14 w-11/12 items-center rounded-full bg-gray-300 px-4';
+
+export const SubsSearchBar = (props) => {
+  const { className } = props;
+  
+  return (
+    <div className={`${stylesSubsSearchBar} ${className}`}>
+      <input
+        type='text'
+        placeholder='Buscar'
+        className='h-10 w-11/12 rounded-full bg-gray-300 pl-4 focus:outline-none'
+      />
+      <div className='flex h-10 w-1/12 items-center justify-center'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth='1.5'
+          stroke='currentColor'
+          className='size-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
--- src/pages/subscriptions/SubsHome.jsx
+++ src/pages/subscriptions/SubsHome.jsx
@@ -1,9 +1,12 @@
 import { SubsCard } from '@/components/SubsCard';
+import { SubsSearchBar } from '@/components/SubsSearchBar';
 
 export const SubsHome = () => {
   return (
     <>
-      <SubsCard className='m-auto' />
+      <SubsCard className='m-auto mb-6' />
+      <SubsSearchBar className='my-6' />
+      
     </>
   );
 };
